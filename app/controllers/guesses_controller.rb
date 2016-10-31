16afb1b74a97a26f1bbf33349c81855919e10626
Dan <danh@users.noreply.github.com>
2016-10-30 22:49:37 -0500

    first try

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,10 +1,14 @@
 class GuessesController < ActionController::Base
 
 def index
+
+  g=Guess.new
+  
+  @list=Guess.all
   @first=params["first_number"]
   @second = params["second_number"]
   @third = params["third_number"]
-  
+
   render ("guesses/index.html.erb")
 end
 
